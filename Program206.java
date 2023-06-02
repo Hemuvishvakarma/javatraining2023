417cbaa9dfb0538c488b072545cf61e14c0a5587
Hemu vishwakarma <131242330+Hemuvishvakarma@users.noreply.github.com>
2023-06-02 17:52:24 +0530

    Add files via upload

---
+++ Program206.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Program206
+{
+public static void main(String args[])
+{
+   char ch = '}';
+   Scanner sc =new Scanner(System.in);
+   ch = sc.next().charAt(0);
+   System.out.println();
+int ascii = ch;
+System.out.println("The ASCII value of " + ch
+                           + " is: " + ascii);
+}
+}
+
+
+
+
